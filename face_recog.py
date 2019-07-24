01efb88673f32b06accefa725a23e2223ec79460
koreafuture <41562375+koreafuture@users.noreply.github.com>
2019-07-24 21:41:48 +0900

    compare
    거리비교를 위한 compare 함수 생성

--- face_recog.py
+++ face_recog.py
@@ -2,7 +2,6 @@
 
 import face_recognition
 import cv2
-import camera
 import os
 import numpy as np
 
@@ -11,7 +10,7 @@ class FaceRecog():
         # Using OpenCV to capture from device 0. If you have trouble capturing
         # from a webcam, comment the line below out and use a video file
         # instead.
-        self.camera = camera.VideoCamera()
+        self.camera = cv2.VideoCapture(0)
 
         self.known_face_encodings = []
         self.known_face_names = []
@@ -38,14 +37,38 @@ class FaceRecog():
     def __del__(self):
         del self.camera
 
+    def compare(self, a):
+        global tempsize
+        if tempsize == []:
+            tempsize.append(a)
+            print(tempsize[0],"off")
+        elif tempsize != []:
+            if tempsize[0] < (a):
+                print("저장값",tempsize)
+                print(a)
+                print("대상이 가까워졌습니다")
+                del tempsize[0]
+                tempsize.append(a)
+
+            elif tempsize[0] > a:
+                print(tempsize)
+                print("대상이 멀어졌습니다")
+                del tempsize[0]
+                tempsize.append(a)
+        return tempsize
+
+    '''def savedistance(self, temp):
+        self.tempsize = []
+        return tempsize'''
     def get_frame(self):
+        global tempsize
         hog = cv2.HOGDescriptor()
 
         hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
         # cv2.startWindowThread()
 
         # Grab a single frame of video
-        frame = self.camera.get_frame()
+        (grabbed, frame) = self.camera.read()
 
         # Resize frame of video to 1/4 size for faster face recognition processing
 
@@ -88,14 +111,18 @@ class FaceRecog():
         # for (x, y, w, h) in self.body:
         #     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
 
+
         for (xA, yA, xB, yB) in self.boxes:
             # display the detected boxes in the colour picture
             cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
         # Display the results
+
+
+
         for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
 
-
-
+            a=right-left
+            tempsize=self.compare(a)
             # Draw a box around the face
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
@@ -104,7 +131,13 @@ class FaceRecog():
             font = cv2.FONT_HERSHEY_DUPLEX
             cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
+
+            #print((left + right)/2, (top + bottom)/2)
+            #print((right - left), (bottom - top))
+
         return frame
+
+
 
     def get_jpg_bytes(self):
         frame = self.get_frame()
@@ -116,6 +149,7 @@ class FaceRecog():
 
 
 if __name__ == '__main__':
+    tempsize = []
     face_recog = FaceRecog()
     print(face_recog.known_face_names)
     while True:
