f71f7b9eaa9d49e31a9994d008f6c51732b9ef3a
moonyeol <ansduf05@gmail.com>
2019-08-07 20:21:28 +0900

    수정

--- test2.cpp
+++ test2.cpp
@@ -27,32 +27,86 @@
     2011.  SSE4 is the next fastest and is supported by most current machines.
 */
 
-#include <dlib/opencv.h>
+
 #include <opencv4/opencv2/highgui/highgui.hpp>
+#include <opencv4/opencv2/highgui.hpp>
 #include <opencv4/opencv2/videoio.hpp>
 #include <opencv4/opencv2/imgproc.hpp>
 #include <opencv4/opencv2/objdetect.hpp>
+#include <opencv4/opencv2/imgproc.hpp>
+#include <opencv4/opencv2/highgui.hpp>
+#include <opencv4/opencv2/dnn.hpp>
+#include <opencv4/opencv2/core.hpp>
+#include <opencv4/opencv2/dnn/dict.hpp>
+#include <opencv4/opencv2/dnn/layer.hpp>
+#include <opencv4/opencv2/dnn/dnn.inl.hpp>
+#include <opencv4/opencv2/dnn/dnn.hpp>
+#include <dlib/opencv.h>
 #include <dlib/image_processing/frontal_face_detector.h>
 #include <dlib/image_processing/render_face_detections.h>
 #include <dlib/image_processing.h>
-
+#include <dlib/clustering.h>
+#include <dlib/string.h>
+#include <dlib/image_io.h>
 #include <dlib/gui_widgets.h>
-
+#include <dlib/dnn.h>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+using namespace cv;
+using namespace cv::dnn;
 using namespace dlib;
 using namespace std;
 
+
+template <template <int,template<typename>class,int,typename> class block, int N, template<typename>class BN, typename SUBNET>
+using residual = add_prev1<block<N,BN,1,tag1<SUBNET>>>;
+
+
+template <template <int,template<typename>class,int,typename> class block, int N, template<typename>class BN, typename SUBNET>
+using residual_down = add_prev2<avg_pool<2,2,2,2,skip1<tag2<block<N,BN,2,tag1<SUBNET>>>>>>;
+
+
+template <int N, template <typename> class BN, int stride, typename SUBNET>
+using block  = BN<con<N,3,3,1,1,relu<BN<con<N,3,3,stride,stride,SUBNET>>>>>;
+
+
+template <int N, typename SUBNET> using ares      = relu<residual<block,N,affine,SUBNET>>;
+template <int N, typename SUBNET> using ares_down = relu<residual_down<block,N,affine,SUBNET>>;
+
+
+template <typename SUBNET> using alevel0 = ares_down<256,SUBNET>;
+template <typename SUBNET> using alevel1 = ares<256,ares<256,ares_down<256,SUBNET>>>;
+template <typename SUBNET> using alevel2 = ares<128,ares<128,ares_down<128,SUBNET>>>;
+template <typename SUBNET> using alevel3 = ares<64,ares<64,ares<64,ares_down<64,SUBNET>>>>;
+template <typename SUBNET> using alevel4 = ares<32,ares<32,ares<32,SUBNET>>>;
+
+
+using anet_type = loss_metric<fc_no_bias<128,avg_pool_everything<
+        alevel0<
+        alevel1<
+        alevel2<
+        alevel3<
+        alevel4<
+        max_pool<3,3,2,2,relu<affine<con<32,7,7,2,2,
+                input_rgb_image_sized<150>
+        >>>>>>>>>>>>;
+
+
 int main()
 {
-    cv::Mat frame;
-    cv::VideoCapture cap;
-    cap.open(0); // 노트북 카메라는 cap.open(1) 또는 cap.open(-1)
-    // USB 카메라는 cap.open(0);
-    int x1;
-    int y1;
-    int x2;
-    int y2;
+
     try
     {
+        cv::Mat frame;
+        cv::VideoCapture cap;
+        cap.open(0); // 노트북 카메라는 cap.open(1) 또는 cap.open(-1)
+        // USB 카메라는 cap.open(0);
+        int x1;
+        int y1;
+        int x2;
+        int y2;
 
         image_window win;
 
@@ -60,6 +114,21 @@ int main()
         frontal_face_detector detector = get_frontal_face_detector();
         shape_predictor pose_model;
         deserialize("shape_predictor_68_face_landmarks.dat") >> pose_model;
+
+
+        anet_type net;
+        deserialize("models/dlib_face_recognition_resnet_model_v1.dat") >> net;
+
+        String prototxt = "mobilenet_ssd/MobileNetSSD_deploy.prototxt";
+        String model = "mobilenet_ssd/MobileNetSSD_deploy.caffemodel";
+        String classesFile = "names";
+        ifstream ifs(classesFile.c_str());
+        string line;
+        std::vector<string> classes;
+        while(getline(ifs,line))
+            classes.push_back(line);
+
+        Net net1 = readNetFromCaffe(prototxt, model);
 
         // Grab and process frames until the main window is closed by the user.
         while(cap.isOpened())
@@ -76,12 +145,19 @@ int main()
             // to reallocate the memory which stores the image as that will make cimg
             // contain dangling pointers.  This basically means you shouldn't modify temp
             // while using cimg.
-            cv_image<bgr_pixel> cimg(frame);
+            cv_image<rgb_pixel> cimg(frame);
 
             // Detect faces
-            std::vector<rectangle> faces = detector(cimg);
+            std::vector<dlib::rectangle> faces = detector(cimg);
             // Find the pose of each face.
             std::vector<full_object_detection> shapes;
+
+//            for(auto face: faces){
+//                auto shape = pose_model(cimg, face);
+//
+//
+//            }
+
             for (unsigned long i = 0; i < faces.size(); ++i) {
 //                shapes.push_back(pose_model(cimg, faces[i]));
                 x1 = faces[i].left();
