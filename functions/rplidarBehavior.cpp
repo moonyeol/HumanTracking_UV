853a3baf965af0bb76b63624a10b29f3372b2ab9
Gihawn Ko <44629526+GKO95@users.noreply.github.com>
2019-09-24 21:11:27 +0900

    방향성 변경 및 신호를 문자열 수신으로 변경.

--- functions/rplidarBehavior.cpp
+++ functions/rplidarBehavior.cpp
@@ -1,19 +1,20 @@
+// UGV 프레임을 제작할 때 RPLIDAR A1 센서를 거꾸로 달아버린 탓에, 방향성을 전부 반대로 설정
 #include <iostream>
 #define DIRECTION 4
 
-#define LEFT 'l'
-#define RIGHT 'r'
-#define FRONT 'g'
-#define BACK 'b'
-#define STOP 's'
+#define LEFT "l"
+#define RIGHT "r"
+#define GO "g"
+#define BACK "b"
+#define STOP "s"
 
-char outputBehavior(/*char, */char, int*);
+char* outputBehavior(/*char, */char*, int*);
 
 int main() {
 
     int arr[4] = {250, 600, 700, 500};
 
-    char direction = outputBehavior(FRONT, arr);
+    char* direction = outputBehavior(GO, arr);
 
     std::cout << direction << std::endl;
 
@@ -21,7 +22,7 @@ int main() {
 }
 
 
-char outputBehavior(/*char detectPosition, */char platformMove, int *distanceRPLIDAR) {
+char* outputBehavior(/*char detectPosition, */char* platformMove, int *distanceRPLIDAR) {
 
     // REFERENCE
     /*
@@ -37,25 +38,26 @@ char outputBehavior(/*char detectPosition, */char platformMove, int *distanceRPL
     #define DIST_REF 500
 
     // 전방에 장애물이 존재할 경우 (0은 측정범위 밖).
-    if (0 < *distanceRPLIDAR && *distanceRPLIDAR <= DIST_STOP){
+    if (0 < *(distanceRPLIDAR + DIRECTION/2) && *(distanceRPLIDAR + DIRECTION/2) <= DIST_STOP){
 
         // 전 방향의 거리여부를 앞에서부터 뒤로 좌우를 동시에 확인한다 (후방 제외).
-        for (int i = 1; i < DIRECTION/2; i++){
+        for (int i = (DIRECTION/2) -1; i > 0; i--){
 
             // 오른쪽이 정해진 기준보다 거리적 여유가 있는 동시에, 왼쪽보다 거리적 여유가 많을 시 오른쪽으로 회전한다.
             if ((*(distanceRPLIDAR + i) > DIST_REF && *(distanceRPLIDAR + i) > *(distanceRPLIDAR + (DIRECTION - i))) || *(distanceRPLIDAR + i) == 0)
-                return RIGHT;
+                return LEFT;
             // 반면 왼쪽이 정해진 기준보다 거리적 여유가 있는 동시에, 오른쪽보다 거리적 여유가 많을 시에는 왼쪽으로 회전한다.
             else if((*(distanceRPLIDAR + (DIRECTION - i)) > DIST_REF  && *(distanceRPLIDAR + i) < *(distanceRPLIDAR + (DIRECTION - i))) || *(distanceRPLIDAR + (DIRECTION - i)) == 0 )
-                return LEFT;
+                return RIGHT;
         }
 
         // 위의 조건문을 만족하지 않았다는 것은 정해진 기준의 여유보다 거리가 적다는 의미이다.
 
         // 후방 거리여부를 확인하고, 전방향이 막혀 있으면 움직이지 않는다.
-        if (*(distanceRPLIDAR + (DIRECTION/2)) <= DIST_REF) return platformMove = BACK;
-        else return platformMove = STOP;
+        if (*(distanceRPLIDAR) <= DIST_REF) return BACK;
+        else return STOP;
     }
+    else if (platformMove != LEFT || platformMove != RIGHT) return GO;
     
     return platformMove;
 }
