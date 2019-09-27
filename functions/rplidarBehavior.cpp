8e0230fb3491c6c1b2dfebe165b172c584e4c7b7
Gihawn Ko <44629526+GKO95@users.noreply.github.com>
2019-09-27 09:19:12 +0900

    Added to BACK when distance is out-of-range.

--- functions/rplidarBehavior.cpp
+++ functions/rplidarBehavior.cpp
@@ -54,7 +54,7 @@ char* rplidarBehavior(/*char detectPosition, */char* platformMove, int *distance
         // 위의 조건문을 만족하지 않았다는 것은 정해진 기준의 여유보다 거리가 적다는 의미이다.
 
         // 후방 거리여부를 확인하고, 전방향이 막혀 있으면 움직이지 않는다.
-        if (*(distanceRPLIDAR) <= DIST_REF) return BACK;
+        if (*(distanceRPLIDAR) <= DIST_REF || *(distanceRPLIDAR) == 0) return BACK;
         else return STOP;
     }
     else if (platformMove != LEFT || platformMove != RIGHT) return GO;
