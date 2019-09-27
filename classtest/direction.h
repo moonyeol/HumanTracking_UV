a6ff819371032926ea2f8b2ed74e11b7d98c3edb
koreafuture <41562375+koreafuture@users.noreply.github.com>
2019-09-27 17:20:07 +0900

    Add files via upload

---
+++ classtest/direction.h
@@ -0,0 +1,18 @@
+#ifndef DIRECTION_H
+#define DIRECTION_H
+
+class direction{
+    
+    public:       
+        direction();
+         char *compare(long left, long right);      
+
+    private:
+        char *move;
+        long xcenter,tempsize = 0 ,size;
+       
+        
+
+    
+};
+#endif
