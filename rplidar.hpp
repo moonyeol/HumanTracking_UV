6f49ef386b6eb02a44b584145b65116a8a475310
Gihawn Ko <44629526+GKO95@users.noreply.github.com>
2019-11-01 23:00:33 +0900

    u_result INFO INCLUDED

--- rplidar.hpp
+++ rplidar.hpp
@@ -31,6 +31,23 @@ class rplidar{
         char* platformMOVE;
 
         // RPLIDAR A1 센서와 드라이버와의 통신 결과를 담는다.
+        /*
+            >> Reference: http://docs.ros.org/kinetic/api/rplidar_ros/html/rptypes_8h_source.html (LINE: 100)
+            >> type defined: uint32_t
+            >> OK:                   0 ->   0.  0.  0.  0
+            >> FAIL_BIT:    0x80000000 -> 128.  0.  0.  0
+             - INVALID DATA:   |0x8000 -> 128.  0.128.  0
+             - OPERATION FAIL: |0x8001 -> 128.  0.128.  1
+             - TIMEOUT         |0x8002 -> 128.  0.128.  2
+             - STOPPED         |0x8003 -> 128.  0.128.  3
+             - NOT SUPPORTED:  |0x8004 -> 128.  0.128.  4
+             - INVALID FORM :  |0x8005 -> 128.  0.128.  5
+             - INSUFF MEMORY:  |0x8006 -> 128.  0.128.  6
+            >> ALREADY_DONE:      0x20 ->   0.  0.  0. 32
+            
+            >> IS_OK(x):   BOOL ((x & FAIL_BIT) == 0) [TRUE WHEN x IS 0]
+            >> IS_FAIL(x): BOOL  (x & FAIL_BIT)       [TRUE WHEN x IS NOT 0]
+        */
         u_result RESULT;
 
         // 스캔 데이터인 노드(node)를 담을 수 있는 배열을 생성한다.
