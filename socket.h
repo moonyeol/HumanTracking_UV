27ec8fdd0d0ceb83d8df5f7c3a79032da95f45fd
koreafuture <41562375+koreafuture@users.noreply.github.com>
2019-11-07 13:38:46 +0900

    socket
    소켓통신 추가 헤더파일 포함

---
+++ socket.h
@@ -0,0 +1,11 @@
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <netinet/in.h>
+# include <sys/socket.h>
+# include <arpa/inet.h>
+# include <unistd.h>
+ 
+# define PORT 20162
+# define BUFFER_SIZE 4096 // 서버에 이용
+# define BUFF_SIZE 100 // 클라이언트에 이용
