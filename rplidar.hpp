1a5212eee5a278544804780513f3a6379177a59d
Gihawn Ko <44629526+GKO95@users.noreply.github.com>
2019-11-15 16:35:19 +0900

    RESTRUCTURED CLASS

--- rplidar.hpp
+++ rplidar.hpp
@@ -11,43 +11,28 @@ class rplidar{
         rplidar();
         ~rplidar();
 
-        // RPLIDAT A1 센서로 한 사이클 스캔한다.
-        void scan();
-    
         // RPLIDAR A1 센서 스캔 결과를 가져온다.
-        void retrieve();
+        void scan();
 
         // 우선순위 결정 후 최종적으로 보내줄 이동신호를 반환한다.
-        char* returnMove(char*);
+        char* move(char*);
 
         // 계산된 거리와 최종 이동방향을 보여준다.
         void result();
 
     private:
         RPlidarDriver* rplidarDRIVER;
+    
+        float rplidarDIST[DIRECTION] = {-1};
 
         // RPLIDAR A1에서 측정한 거리와 플랫폼 이동신호를 담는 변수이다.
-        int rplidarDIST[DIRECTION] = {0};
+        // int rplidarDIST[DIRECTION] = {0};
         char* platformMOVE;
 
+        // AVOID OBSTACLE
+        bool avoid;
+
         // RPLIDAR A1 센서와 드라이버와의 통신 결과를 담는다.
-        /*
-            >> Reference: http://docs.ros.org/kinetic/api/rplidar_ros/html/rptypes_8h_source.html (LINE: 100)
-            >> type defined: uint32_t
-            >> OK:                   0 ->   0.  0.  0.  0
-            >> FAIL_BIT:    0x80000000 -> 128.  0.  0.  0
-             - INVALID DATA:   |0x8000 -> 128.  0.128.  0
-             - OPERATION FAIL: |0x8001 -> 128.  0.128.  1
-             - TIMEOUT         |0x8002 -> 128.  0.128.  2
-             - STOPPED         |0x8003 -> 128.  0.128.  3
-             - NOT SUPPORTED:  |0x8004 -> 128.  0.128.  4
-             - INVALID FORM :  |0x8005 -> 128.  0.128.  5
-             - INSUFF MEMORY:  |0x8006 -> 128.  0.128.  6
-            >> ALREADY_DONE:      0x20 ->   0.  0.  0. 32
-            
-            >> IS_OK(x):   BOOL ((x & FAIL_BIT) == 0) [TRUE WHEN x IS 0]
-            >> IS_FAIL(x): BOOL  (x & FAIL_BIT)       [TRUE WHEN x IS NOT 0]
-        */
         u_result RESULT;
 
         // 스캔 데이터인 노드(node)를 담을 수 있는 배열을 생성한다.
@@ -58,7 +43,8 @@ class rplidar{
         void compressDistance();
 
         // RPLIDAR 거리와 플랫폼 이동 신호를 통합하여 우선순위를 결정한다.
-        void behavior(char*);
+        char* behavior(char*);
+
 };
 
 #endif
